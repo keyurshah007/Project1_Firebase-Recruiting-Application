8c2c023261465bab6ee55e780412338f86952e08
keyurshah007 <keyurshah247@gmail.com>
2024-03-17 10:59:39 -0400

    second commit with modified layouts

---
+++ app/src/main/java/com/example/project_part1/Candidate.kt
@@ -0,0 +1,5 @@
+
+
+package com.example.project_part1
+
+data class Candidate(val name: String = "", val image: String = "")
---
+++ app/src/main/java/com/example/project_part1/CandidateAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.project_part1
+
+
+class CandidateAdapter {
+}
