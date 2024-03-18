3e80a3429381ba226c4be312040aba16c8c38622
keyurshah007 <keyurshah247@gmail.com>
2024-03-17 22:19:05 -0400

    recyclerview with layouts

--- app/src/main/java/com/example/project_part1/CandidateActivity.kt
+++ app/src/main/java/com/example/project_part1/CandidateActivity.kt
@@ -1,11 +1,45 @@
 package com.example.project_part1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.FirebaseDatabase
+import com.firebase.ui.database.FirebaseRecyclerOptions
 
 class CandidateActivity : AppCompatActivity() {
+
+    private var adapter: CandidateAdapter? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_candidate)
+//
+//        val query = FirebaseDatabase.getInstance().reference.child("Candidates")
+//        val options = FirebaseRecyclerOptions.Builder<Candidate>().setQuery(query, Candidate::class.java).build()
+//
+//        adapter = CandidateAdapter(options)
+//        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewCandidate)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = adapter
+//    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_candidate)
+
+        val query = FirebaseDatabase.getInstance().reference.child("Candidates")
+        val options = FirebaseRecyclerOptions.Builder<Candidate>().setQuery(query, Candidate::class.java).build()
+
+        adapter = CandidateAdapter(options)
+        val rView: RecyclerView = findViewById(R.id.recyclerViewCandidate)
+        rView.layoutManager = LinearLayoutManager(this)
+        rView.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter?.startListening()
     }
 }
--- app/src/main/java/com/example/project_part1/CandidateAdapter.kt
+++ app/src/main/java/com/example/project_part1/CandidateAdapter.kt
@@ -1,5 +1,66 @@
 package com.example.project_part1
 
 
-class CandidateAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+
+//class CandidateAdapter(options: FirebaseRecyclerOptions<Candidate>) :
+//    FirebaseRecyclerAdapter<Candidate, CandidateAdapter.ViewHolder>(options) {
+//
+//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val name: TextView = itemView.findViewById(R.id.txtName)
+//        val image: ImageView = itemView.findViewById(R.id.imageViewImage)
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.candidateitem, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Candidate) {
+//        holder.name.text = model.name
+//        Glide.with(holder.image.context).load(model.image).into(holder.image)
+//    }
+
+
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import android.widget.ImageView
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//import com.bumptech.glide.Glide
+//import com.firebase.ui.database.FirebaseRecyclerAdapter
+//import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+class CandidateAdapter(options: FirebaseRecyclerOptions<Candidate>) :
+    FirebaseRecyclerAdapter<Candidate, CandidateAdapter.MyViewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int, model: Candidate) {
+        val storRef: StorageReference = FirebaseStorage.getInstance().getReferenceFromUrl(model.image)
+        Glide.with(holder.img.context).load(storRef).into(holder.img)
+        holder.txtName.text = model.name
+        
+    }
+
+    class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.candidateitem, parent, false)) {
+        val txtName: TextView = itemView.findViewById(R.id.txtName)
+
+        val img: ImageView = itemView.findViewById(R.id.imgCandidate)
+    }
 }
+
